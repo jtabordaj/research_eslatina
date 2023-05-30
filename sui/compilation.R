174ff313103108ecbd01e7bd2dfa6c626b64e97b
jtabordaj <jtabordaj@uninorte.edu.co>
2023-05-30 13:49:03 -0500

    some progress

--- sui/compilation.R
+++ sui/compilation.R
@@ -1,26 +1,28 @@
 source("./sui/dependencies.R")
 
 readFiles(2021, 1, 12, "energia", "pcon")
-
 allDataFrames <- names(which(unlist(eapply(.GlobalEnv, is.data.frame))))
-dflist <- list()
 
+dflist <- list()
 for(df in allDataFrames){
     dflist[[df]] <- get(df)
 }
+dflist <- lapply(dflist, formatNAs)
+dflist <- lapply(dflist, formatNumbers)
 
 
+output_text <- data.frame(matrix(ncol = 16, nrow = nrow(dflist[[1]])))
+output_text[, 1:4] <- dflist[[1]][, 1:4]
 
+output_numeric <- data.frame(matrix(ncol = 12, nrow = nrow(dflist[[1]])))
 
-# Define a custom merge function to sum numeric rows
-merge_and_sum <- function(x, y) {
-  merged <- merge(x, y, by = c("row_names", "col_names"), all = TRUE)
-  merged[, -c(1, 2)] <- rowSums(merged[, -c(1, 2)], na.rm = TRUE)
-  return(merged)
+## Iterator
+
+for (i in seq_along(dflist)) {
+  aligned_rows <- match(rownames([dflist[[1]]]), rownames(dflist[[i]]))
+  output_numeric <- output_numeric + dflist[[i]][aligned_rows, 5:16]
 }
+output_numeric <- output_numeric / 12
+output_text[, 5:16] <- output_numeric
+output <- output_text
 
-# Merge the data frames and sum the numeric rows
-merged_df <- Reduce(merge_and_sum, df_list)
-
-# Print the merged data frame
-print(merged_df)
--- sui/dependencies.R
+++ sui/dependencies.R
@@ -14,6 +14,16 @@ readFiles <- function(year, monthStart, monthEnd, type, variable){
     }
 }
 
+formatNAs <- function(df){
+  df[df == "ND"] <- NA
+  return(df)
+}
+
+formatNumbers <- function(df){
+    df[, 5:16] <- lapply(df[, 5:16], as.numeric)
+    return(df)
+}
+
 mergeAndSum <- function(x, y){
     merged <- merge(x, y, by = c("row_names", "col_names"), all = TRUE)
     merged[, -c(1, 2)] <- rowSums(merged[, -c(1, 2)], na.rm = TRUE)
