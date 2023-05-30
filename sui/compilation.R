f6ea505de2c337954d205df3d82c42405ad9834e
jtabordaj <jtabordaj@uninorte.edu.co>
2023-05-30 17:41:33 -0500

    managed to push one value

--- sui/compilation.R
+++ sui/compilation.R
@@ -3,6 +3,22 @@ source("./sui/dependencies.R")
 readFiles(2021, 1, 12, "energia", "pcon")
 allDataFrames <- names(which(unlist(eapply(.GlobalEnv, is.data.frame))))
 
+df <- pcon2021_1
+
+colnames(df) <- varnames
+df <- df %>% formatNumbers()
+df <- df %>% formatRegion()
+
+numeric_interval <- 11
+df[, 5:16][is.na(df[, 5:16])] <- NA
+df <- df[complete.cases(df[, numeric_interval]),]
+
+ejemplo <- df %>% subset(Departamento == "ANTIOQUIA" & Municipio == "BELLO")
+muni <- df %>% distinct(Departamento, Municipio)
+muni <- muni %>% mutate(totResidencial == mean(ejemplo$totResidencial))
+#
+
+######
 dflist <- list()
 for(df in allDataFrames){
     dflist[[df]] <- get(df)
@@ -10,5 +26,3 @@ for(df in allDataFrames){
 
 dflist <- lapply(dflist, formatNAs)
 dflist <- lapply(dflist, formatNumbers)
-
-summary(dflist[[1]])
--- sui/dependencies.R
+++ sui/dependencies.R
@@ -7,6 +7,11 @@ library(writexl)
 library(haven)
 library(purrr)
 
+varnames <- c("Departamento", "Municipio", "Empresa", "Variable", 
+    "Estrato1", "Estrato2", "Estrato3", "Estrato4", "Estrato5", "Estrato6",
+    "totResidencial", "Industrial", "Comercial", "Oficial", "Otros", "totNoResidencial"
+)
+
 readFiles <- function(year, monthStart, monthEnd, type, variable){
     for(i in monthStart:monthEnd){
         path <- paste("./sui/data/",type,"/",year,"/",variable,"/",i,".csv", sep = "")
@@ -15,13 +20,18 @@ readFiles <- function(year, monthStart, monthEnd, type, variable){
 }
 
 formatNAs <- function(df){
-  df[df == "ND"] <- NA
-  return(df)
+    df[is.na(df)] <- 0
+    return(df)
 }
 
 formatNumbers <- function(df) {
   df[, 5:16] <- lapply(df[, 5:16], as.numeric)
   return(df)
+}
+
+formatRegion <- function(df){
+    df[, 1:4] <- lapply(df[, 1:4], as.factor)
+    return(df)
 }
 
 mergeAndSum <- function(x, y){
