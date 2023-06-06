6eb831e1578b3eba87d2d9456198bf0b6641bc00
jtabordaj <jtabordaj@uninorte.edu.co>
2023-06-05 19:55:59 -0500

    full database for 2021 generated

--- sui/app.R
+++ sui/app.R
@@ -1,12 +1,19 @@
 source("./sui/dependencies.R")
 
-readOutputs(2021, "energia")
+year <- 2021
+type <- "energia"
+readOutputs(year, "energia")
 
 merge_criteria <- c("Departamento", "Municipio")
-df <- left_join(get(paste(variable,year,"_1", sep = "")), get(paste(variable,year,"_2", sep = "")), by = merge_criteria )
+frameNames <- c("vcon", "tcon", "sus", "ptar", "pfac")
 
-frameNames <- c("vcon", "tcon", "sus", "ptar", "pfac", "pcon", "fac")
+df <- left_join(get(paste("fac_",year, sep = "")), get(paste("pcon_",year, sep = "")), by = merge_criteria)
 
-for(i in 3:12){
-    df <- left_join(df, get(paste(variable,year,"_",i, sep = "")), by = merge_criteria )
+for(i in frameNames){
+    df <- left_join(df, get(paste(i,"_",year, sep = "")), by = merge_criteria )
 }
+
+View(df)
+path <- paste("./sui/data/",type,"/",year,"/",year,"_fulldb.xlsx", sep = "")
+write_xlsx(df, path)
+paste("Wrote file")
