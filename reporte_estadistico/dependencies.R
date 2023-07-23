8c50f363982caa735d1468cf238e9c83d4a66c0d
jtabordaj <jtabordaj@uninorte.edu.co>
2023-07-22 20:49:42 -0500

    added grab metrics bulk function

--- reporte_estadistico/dependencies.R
+++ reporte_estadistico/dependencies.R
@@ -25,41 +25,57 @@ createMetrics <- function(df, metric) {
         if (tolower(metric) == "mean") {
             calculatedMetric <- mean(df[, i], na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], mean = calculatedMetric)
         } else if (tolower(metric) == "variance") {
             calculatedMetric <- var(df[, i], na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], variance = calculatedMetric)
         } else if (tolower(metric) == "standard_deviation") {
             calculatedMetric <- sd(df[, i], na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], std_dev = calculatedMetric)
         } else if (tolower(metric) == "median") {
             calculatedMetric <- median(df[, i], na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], median = calculatedMetric)
         } else if(tolower(metric) == "kurtosis"){
-            lowerCutoff <- quantile(df[,i], probs = 10/100)
-            upperCutoff <- quantile(df[,i], probs = 1 - 10/100)
+            lowerCutoff <- quantile(df[,i], probs = 10/100, na.rm = TRUE)
+            upperCutoff <- quantile(df[,i], probs = 1 - 10/100, na.rm = TRUE)
             trimmed_column <- df[df[, i] >= lowerCutoff & df[, i] <= upperCutoff, i]
             calculatedMetric <- kurtosis(trimmed_column, na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], kurtosis = calculatedMetric)
         } else if(tolower(metric) == "skewness"){
-            lowerCutoff <- quantile(df[,i], probs = 10/100)
-            upperCutoff <- quantile(df[,i], probs = 1 - 10/100)
+            lowerCutoff <- quantile(df[,i], probs = 10/100, na.rm = TRUE)
+            upperCutoff <- quantile(df[,i], probs = 1 - 10/100, na.rm = TRUE)
             trimmed_column <- df[df[, i] >= lowerCutoff & df[, i] <= upperCutoff, i]
             calculatedMetric <- kurtosis(trimmed_column, na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], ":", calculatedMetric, sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric = calculatedMetric)
+            row <- data.frame(name = colnames(df)[i], skewness = calculatedMetric)
         } else if(tolower(metric) == "quantiles"){
             calculatedMetric <- quantile(df[, i], na.rm = TRUE)
             results[i - 2] <- paste(metric, "value of", colnames(df)[i], "in order from 0 to 100%:", calculatedMetric[1], calculatedMetric[2], calculatedMetric[3],calculatedMetric[4], metric0 = calculatedMetric[5], sep = " ")
-            row <- data.frame(name = colnames(df)[i], metric0 = calculatedMetric[1], metric25 = calculatedMetric[2], metric50 = calculatedMetric[3], metric75 = calculatedMetric[4], metric100 = calculatedMetric[5])
+            row <- data.frame(name = colnames(df)[i], quant0 = calculatedMetric[1], quant25 = calculatedMetric[2], quant50 = calculatedMetric[3], quant75 = calculatedMetric[4], quant100 = calculatedMetric[5])
         }
         output <- rbind(output, row)
     }
     return(list(results = results, output = output))
+}
+
+voi <- c("mean", "variance", "standard_deviation", "median", "kurtosis", "skewness", "quantiles")
+
+initializeMetricsDF <- function(df) {
+  rowNames <- data.frame(colnames(df[3:10]))
+  allMetrics <- data.frame(names = rowNames)
+  assign("metricsDF", allMetrics, envir = .GlobalEnv)
+}
+
+grabMetrics <- function(df, metricsDF) {
+  for (i in voi) {
+    obj <- createMetrics(df, i)$output
+    metricsDF <- inner_join(obj, metricsDF, by = "name")
+  }
+  return(metricsDF)  
 }
 
 ## For kurtosis and skewness, the function drops lower 10 and upper 10
--- reporte_estadistico/energia.R
+++ reporte_estadistico/energia.R
@@ -6,5 +6,15 @@ data21 <- read_xlsx('./reporte_estadistico/data/energia/2021_fulldb.xlsx')
 data22 <- read_xlsx('./reporte_estadistico/data/energia/2022_fulldb.xlsx')
 
 spliceVariables(data21)
-createMetrics(data_fac, "quantiles")$output
+allDf <- ls()
+dfList <- allDf[grep("^data_", allDf)]
+
+createMetrics(data_fac, "mean")
+
+initializeMetricsDF(data_fac)
+colnames(metricsDF) <- c("name")
+metricsDF <- grabMetrics(data_fac, metricsDF)
+
+
+
 
