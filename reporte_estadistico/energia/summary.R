88ccf539d2c039ab75e1b65eb064c3c85d80959a
jtabordaj <jtabordaj@uninorte.edu.co>
2023-08-28 19:11:28 -0500

    added writing

---
+++ reporte_estadistico/energia/summary.R
@@ -0,0 +1,4 @@
+source("./reporte_estadistico/energia/dependencies.R")
+
+data21 <- read_xlsx('./reporte_estadistico/data/energia/2021_fulldb.xlsx')
+data22 <- read_xlsx('./reporte_estadistico/data/energia/2022_fulldb.xlsx')
