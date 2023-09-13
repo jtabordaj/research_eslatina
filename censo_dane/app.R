b814cc7e681ce2f90c7a7a32effca89ba138cc37
jtabordaj <jtabordaj@uninorte.edu.co>
2023-09-13 15:14:18 -0500

    added cnpv 13

--- censo_dane/app.R
+++ censo_dane/app.R
@@ -3,7 +3,7 @@ source("./censo_dane/dependencies.R")
 # Santa Lucia, Atlantico - 675
 # Santa Rosa de Lima, Bolivar - 683
 
-divipola <- 8
+divipola <- 13
 readFiles(divipola)
 envir <- ls()
 dfsEnvir <- envir[sapply(envir, function(x) is.data.frame(get(x)))]
